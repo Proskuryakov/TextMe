2c5fc23a65d5cca679f22b33aa60994ea8c64ed4
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-05-04 15:11:10 +0300

    Add main page without logics and nav-panel component

--- frontend/src/app/routed/main/main-routing.module.ts
+++ frontend/src/app/routed/main/main-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainPage} from './pages/main/main.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/routed/main/main.module.ts
+++ frontend/src/app/routed/main/main.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { MainPage } from './pages/main/main.page';
+import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainPage,
+    NavigationPanelComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
