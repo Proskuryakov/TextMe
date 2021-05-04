d9a93a25843796b92cd65923c11abe9968b059b3
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-05-04 14:43:08 +0300

    Add main module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./routed/main/main.module').then(m => m.MainModule)
+  },
+  {
     path: 'start',
     loadChildren: () =>
       import('./routed/start/start.module').then(m => m.StartModule)
