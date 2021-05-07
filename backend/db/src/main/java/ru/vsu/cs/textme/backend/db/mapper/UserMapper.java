0604b892f9b73359a7336e1d521b87a184290b3b
StrangeChestnut <strangechestnut@gmail.com>
2021-05-07 21:11:17 +0300

    Added @Data files

---
+++ backend/db/src/main/java/ru/vsu/cs/textme/backend/db/mapper/UserMapper.java
@@ -0,0 +1,44 @@
+package ru.vsu.cs.textme.backend.db.mapper;
+
+import org.apache.ibatis.annotations.*;
+import ru.vsu.cs.textme.backend.db.model.*;
+
+import java.util.List;
+
+
+@Mapper
+public interface UserMapper {
+    @Select("SELECT * FROM users WHERE email = #{email}")
+    @Results(id = "userResult", value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "nickname", column = "nickname"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "roles", column= "id", javaType = List.class,  many = @Many(select = "findRolesByUserId")),
+    })
+    User findUserByEmail(String email);
+
+    @Select("SELECT * FROM users WHERE nickname = #{nickname}")
+    @ResultMap("userResult")
+    User findUserByNickname(String nickname);
+
+    @Select("SELECT * FROM create_user(#{nickname},#{email},#{password});")
+    @ResultMap("userResult")
+    User createUser(RegistrationRequest user);
+
+    @Select("SELECT f.url FROM files as f WHERE f.id = #{id}")
+    String findAvatarByUserId(String id);
+
+    @Select("SELECT ar.* FROM user_app_role as uar, app_roles as ar WHERE uar.user_id = #{id} AND ar.id = uar.role_id")
+    List<AppRole> findRolesByUserId(int id);
+
+    @Select("SELECT * FROM cards WHERE cards.id = #{id}")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "content", column = "content"),
+            @Result(property = "tags", column = "id", javaType = Tag.class, many = @Many(select = "findTagsByCardId")),
+    })
+    Card findCardByUserId(String id);
+
+    @Select("SELECT tags.* FROM card_tags as ct, tags as t WHERE ct.card_id = #{id} AND t.id = ct.tag_id")
+    List<AppRole> findTagsByCardId(String id);
+}
