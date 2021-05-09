571439c2c0429992357da46b5c98367d21573833
StrangeChestnut <strangechestnut@gmail.com>
2021-05-09 14:54:45 +0300

    Changed dependencies, migrations and config

--- backend/db/src/main/resources/db/migration/v1.0/textme_functions_04052021-2140.sql
+++ backend/db/src/main/resources/db/migration/v1.0/textme_functions_04052021-2140.sql
@@ -7,12 +7,11 @@ DECLARE
     _user users;
     _id INTEGER;
 BEGIN
-    IF EXISTS (SELECT 1 FROM users WHERE nickname = nick OR email = mail)
+    IF EXISTS (SELECT * FROM users WHERE nickname = nick OR email = mail)
     THEN RETURN NULL;
     END IF;
     INSERT INTO cards(content) VALUES (NULL) RETURNING id INTO _id;
     INSERT INTO users(nickname, email, password, card_id) VALUES (nick, mail, pass, _id) RETURNING * INTO _user;
-    INSERT INTO user_app_role(user_id, role_id) VALUES (_id, 0);
     RETURN _user;
 END;
 $$;
@@ -31,4 +30,35 @@ BEGIN
     INSERT INTO user_chat_role(user_id, chat_id, role_id) VALUES (owner_id, _chat.id, 2);
     RETURN _chat;
 END;
+$$;
+
+CREATE OR REPLACE FUNCTION save_avatar(nick VARCHAR, path VARCHAR, type INTEGER)
+    RETURNS users LANGUAGE plpgsql AS
+$$
+DECLARE
+    _id INTEGER;
+BEGIN
+    SELECT _id = image_id FROM users WHERE nickname = nick;
+    IF _id THEN
+        UPDATE files SET url = path, path_type = type WHERE image_id = _id;
+    ELSE
+        INSERT INTO files (url, path_type) VALUES (path, type) RETURNING id AS _id;
+        UPDATE users SET image_id = _id WHERE nickname = nick;
+    END IF;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION activate_email(mail_id VARCHAR, us_id INTEGER)
+    RETURNS VARCHAR LANGUAGE plpgsql AS
+$$
+DECLARE
+    _mail VARCHAR;
+BEGIN
+    DELETE FROM inactive_emails WHERE uuid = mail_id AND user_id = us_id RETURNING email INTO _mail;
+    IF _mail IS NULL THEN
+        RETURN NULL;
+    END IF;
+    UPDATE users SET email = _mail WHERE id = us_id;
+    RETURN _mail;
+END
 $$;
