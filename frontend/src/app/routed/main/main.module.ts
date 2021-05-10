01f2510704622c445d6cf8ea3385bc39269796ca
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-05-10 20:47:21 +0300

    Add computer view for main page

--- frontend/src/app/routed/main/main.module.ts
+++ frontend/src/app/routed/main/main.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainPage } from './pages/main/main.page';
 import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
+import { MainPageUserCardComponent } from './components/main-page-user-card/main-page-user-card.component';
+import { LeftNavbarLogoComponent } from './components/left-navbar-logo/left-navbar-logo.component';
 
 
 @NgModule({
   declarations: [
     MainPage,
-    NavigationPanelComponent
+    NavigationPanelComponent,
+    MainPageUserCardComponent,
+    LeftNavbarLogoComponent
   ],
   imports: [
     CommonModule,
--- frontend/src/app/routed/main/pages/main/main.page.ts
+++ frontend/src/app/routed/main/pages/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   templateUrl: './main.page.html',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigateTo(address: string): void{
+    this.router.navigate([address]);
+  }
 }
