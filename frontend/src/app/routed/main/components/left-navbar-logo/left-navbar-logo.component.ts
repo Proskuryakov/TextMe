f9ea17b85a630034505f7fdfc3a42817064349dd
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-05-10 20:46:44 +0300

    Add navigation to nav-bar and create route link on all nav-panel

---
+++ frontend/src/app/routed/main/components/left-navbar-logo/left-navbar-logo.component.ts
@@ -0,0 +1,14 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-left-navbar-logo',
+  templateUrl: './left-navbar-logo.component.html',
+  styleUrls: ['./left-navbar-logo.component.sass']
+})
+export class LeftNavbarLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
