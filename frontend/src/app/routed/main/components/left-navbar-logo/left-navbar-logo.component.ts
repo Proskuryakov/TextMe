0f7646b07c4789abb515b0faf927822d9cd9c3e3
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-05-11 12:30:19 +0300

    Add logout button in nav-bar for computer view page

--- frontend/src/app/routed/main/components/left-navbar-logo/left-navbar-logo.component.ts
+++ frontend/src/app/routed/main/components/left-navbar-logo/left-navbar-logo.component.ts
@@ -1,4 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../../../core/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-left-navbar-logo',
@@ -7,8 +9,17 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class LeftNavbarLogoComponent implements OnInit {
 
-  constructor() { }
+  imageSize = 50;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) { }
 
   ngOnInit(): void {}
 
+  logout(): void {
+    this.authService.deleteToken();
+    this.router.navigate(['/start']);
+  }
 }
