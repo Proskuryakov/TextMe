235dd3f26afdb7abe0c66ea84f97622bcb792c68
StrangeChestnut <strangechestnut@gmail.com>
2021-06-12 17:26:23 +0300

    Fix controller annotation

--- backend/api/src/main/java/ru/vsu/cs/textme/backend/api/MessengerController.java
+++ backend/api/src/main/java/ru/vsu/cs/textme/backend/api/MessengerController.java
@@ -2,10 +2,7 @@ package ru.vsu.cs.textme.backend.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.vsu.cs.textme.backend.db.model.DirectMessage;
 import ru.vsu.cs.textme.backend.db.model.info.ChatMessageInfo;
 import ru.vsu.cs.textme.backend.security.CustomUserDetails;
@@ -15,7 +12,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.Size;
 import java.util.List;
 
-@RestController("/api/messenger")
+@RestController()
+@RequestMapping("/api/messenger")
 public class MessengerController {
     private final MessengerService messengerService;
 
