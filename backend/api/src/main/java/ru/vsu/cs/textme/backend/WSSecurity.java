d0e5b939fb0dc3095601bb050fd527b3edd16eba
StrangeChestnut <strangechestnut@gmail.com>
2021-06-14 11:01:15 +0300

    Refactoring

--- backend/api/src/main/java/ru/vsu/cs/textme/backend/WSSecurity.java
+++ backend/api/src/main/java/ru/vsu/cs/textme/backend/WSSecurity.java
@@ -1,13 +1,19 @@
 package ru.vsu.cs.textme.backend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import ru.vsu.cs.textme.backend.security.CustomUserDetailsService;
 import ru.vsu.cs.textme.backend.security.JwtProvider;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -36,6 +42,14 @@ public class WSSecurity extends AbstractSecurityWebSocketMessageBrokerConfigurer
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .addInterceptors(new HttpSessionHandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                                   WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                        attributes.put("token", request.getHeaders().getFirst("Authorization"));
+                        return true;
+                    }
+                })
                 .setHandshakeHandler(new WSDefaultHandshakeHandler(provider, detailsService))
                 .setAllowedOriginPatterns("*")
                 .withSockJS()
