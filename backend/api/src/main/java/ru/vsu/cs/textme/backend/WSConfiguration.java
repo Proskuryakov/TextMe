5dccca3ca81c401cfd3efac40a53e4cd23da5e11
StrangeChestnut <strangechestnut@gmail.com>
2021-06-15 00:35:04 +0300

    Debug things

--- backend/api/src/main/java/ru/vsu/cs/textme/backend/WSConfiguration.java
+++ backend/api/src/main/java/ru/vsu/cs/textme/backend/WSConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -13,11 +15,16 @@ import org.springframework.messaging.simp.config.ChannelRegistration;
 
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,6 +43,22 @@ public class WSConfiguration implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        return super.beforeHandshake(request, response, wsHandler, attributes);
+                    }
+
+                    @Override
+                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception ex) {
+                        super.afterHandshake(request, response, wsHandler, ex);
+                    }
+                }).setHandshakeHandler(new DefaultHandshakeHandler() {
+                @Override
+                protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                    return super.determineUser(request, wsHandler, attributes);
+                }
+        })
                 .withSockJS()
                 .setSupressCors(true);
     }
