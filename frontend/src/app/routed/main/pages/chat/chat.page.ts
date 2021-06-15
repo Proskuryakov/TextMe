ef090f40afe71d6d3790b10abc9a657837e061e6
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-15 23:57:56 +0300

    Add testing socket connection for send and receive

--- frontend/src/app/routed/main/pages/chat/chat.page.ts
+++ frontend/src/app/routed/main/pages/chat/chat.page.ts
@@ -1,15 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {
-  DestinationType,
-  Message,
-  MessageInfo,
-  MessageStatus,
-  NewMessageRequest
-} from '../../../../features/chat/models/message.model';
-import {DirectService} from '../../../../features/chat/services/direct.service';
+import {MessageInfo, NewMessageRequest} from '../../../../features/chat/models/message.model';
+import {MessengerSocketService} from '../../../../features/chat/services/messenger-socket.service';
 import {UserApiService} from '../../../../features/profile/services/user-api.service';
 import {Profile} from '../../../../features/profile/models/profile.model';
-import {Info} from '../../../../features/profile/models/info.model';
 
 @Component({
   templateUrl: './chat.page.html',
@@ -17,52 +10,23 @@ import {Info} from '../../../../features/profile/models/info.model';
 })
 export class ChatPage implements OnInit {
 
-  messages: MessageInfo[];
+  messages: MessageInfo[] = [];
   newMessage: string;
   user: Profile;
 
   constructor(
-    private readonly directService: DirectService,
+    private readonly msgSocketService: MessengerSocketService,
     private readonly userApiService: UserApiService
-  ) {
-    userApiService.getCurrentUser().subscribe(
-      user => this.user = user
-    );
-    //
-    // const message = new MessageInfo();
-    // message.destination = DestinationType.CHAT;
-    // message.from = this.user.info;
-    // message.to = new Info();
-    // message.to.id = 2;
-    // message.to.name = 'danila';
-    // message.message = new Message();
-    // message.message.content = 'Привет';
-    // message.message.dateCreate = new Date();
-    // message.message.id = 1;
-    // message.message.status = MessageStatus.READ;
-    //
-    // const message1 = new MessageInfo();
-    // message1.destination = DestinationType.CHAT;
-    // message1.from = new Info();
-    // message1.from.id = 2;
-    // message1.from.name = 'danila';
-    // message1.to = new Info();
-    // message1.to.id = 1;
-    // message1.to.name = 'egor';
-    // message1.message = new Message();
-    // message1.message.content = 'Привет';
-    // message1.message.dateCreate = new Date();
-    // message1.message.id = 2;
-    // message1.message.status = MessageStatus.READ;
-    // this.messages.push(message, message1);
-  }
+  ) {}
 
   ngOnInit(): void {
-    // this.directService.getSendMessage().subscribe(
-    //   (message) => {
-    //     this.messages.push(message);
-    //   }
-    // );
+    this.userApiService.getCurrentUser().subscribe(
+      user => {
+        this.user = user;
+      }
+    );
+    this.msgSocketService.getSendMessage().subscribe((message) => this.messages.push(message));
+    this.msgSocketService.getDirectException().subscribe((exception) => console.log(exception.error));
   }
 
   sendMessage(): void {
@@ -71,7 +35,7 @@ export class ChatPage implements OnInit {
     directMessage.message = this.newMessage;
     directMessage.recipient = 1;
     // TODO убрать тестовый хардкод
-    this.directService.sendMessage(directMessage);
+    this.msgSocketService.directSendMessage(directMessage);
     this.newMessage = '';
   }
 }
