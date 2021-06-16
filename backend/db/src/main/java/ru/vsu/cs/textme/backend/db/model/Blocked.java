9d2c1051ed49df760a3f25af199b54416c31de4a
StrangeChestnut <strangechestnut@gmail.com>
2021-06-16 14:11:52 +0300

    Fix timestamp

--- backend/db/src/main/java/ru/vsu/cs/textme/backend/db/model/Blocked.java
+++ backend/db/src/main/java/ru/vsu/cs/textme/backend/db/model/Blocked.java
@@ -3,10 +3,11 @@ package ru.vsu.cs.textme.backend.db.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
 import java.time.ZonedDateTime;
 
 @Getter @Setter
 public class Blocked {
-    private ZonedDateTime from;
-    private ZonedDateTime to;
+    private Timestamp from;
+    private Timestamp to;
 }
