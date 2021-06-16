92b5efecf68c3fa412bcc7058fae15c2ff61e6cf
StrangeChestnut <strangechestnut@gmail.com>
2021-06-16 18:43:01 +0300

    Set server timezone UTC

--- backend/src/main/java/ru/vsu/cs/textme/backend/BackendApplication.java
+++ backend/src/main/java/ru/vsu/cs/textme/backend/BackendApplication.java
@@ -3,9 +3,12 @@ package ru.vsu.cs.textme.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC"));
         SpringApplication.run(BackendApplication.class, args);
     }
 }
