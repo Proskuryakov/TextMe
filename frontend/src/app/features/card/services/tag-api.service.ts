957f3382db53420956de5c5559bd9e4e041f74ed
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-18 02:33:06 +0300

    Add tag-api services with tag search

--- frontend/src/app/features/card/services/tag-api.service.ts
+++ frontend/src/app/features/card/services/tag-api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../../../environments/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,13 @@ import {environment} from '../../../../environments/environment';
 export class TagApiService {
   tagURL = `${environment.api}/tag`;
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  getTagsLike(begin: string): Observable<string[]> {
+    const params = new HttpParams().set('begin', begin);
+    return this.http.get<string[]>(`${this.tagURL}/like`, {params});
+  }
+
 }
