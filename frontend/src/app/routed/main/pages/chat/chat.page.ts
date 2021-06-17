565757ddb31f8c74937b0a5fee8a2ebbceb1d88f
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-17 20:02:10 +0300

    Add group chat stube

--- frontend/src/app/routed/main/pages/chat/chat.page.ts
+++ frontend/src/app/routed/main/pages/chat/chat.page.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {MessageInfo, NewMessageRequest} from '../../../../features/chat/models/message.model';
-import {MessengerSocketService} from '../../../../features/chat/services/messenger-socket.service';
-import {UserApiService} from '../../../../features/profile/services/user-api.service';
-import {Profile} from '../../../../features/profile/models/profile.model';
 
 @Component({
   templateUrl: './chat.page.html',
@@ -10,32 +6,8 @@ import {Profile} from '../../../../features/profile/models/profile.model';
 })
 export class ChatPage implements OnInit {
 
-  messages: MessageInfo[] = [];
-  newMessage: string;
-  user: Profile;
+  constructor() {}
 
-  constructor(
-    private readonly msgSocketService: MessengerSocketService,
-    private readonly userApiService: UserApiService
-  ) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.userApiService.getCurrentUser().subscribe(
-      user => {
-        this.user = user;
-      }
-    );
-    this.msgSocketService.getSendMessage().subscribe((message) => this.messages.push(message));
-    this.msgSocketService.getDirectException().subscribe((exception) => console.log(exception.error));
-  }
-
-  sendMessage(): void {
-    console.log('НАЖАЛИ КНОПКУ');
-    const directMessage = new NewMessageRequest();
-    directMessage.message = this.newMessage;
-    directMessage.recipient = 1;
-    // TODO убрать тестовый хардкод
-    this.msgSocketService.directSendMessage(directMessage);
-    this.newMessage = '';
-  }
 }
