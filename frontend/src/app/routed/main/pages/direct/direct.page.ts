0755a8a05d247aa3ccde600a463cd1eff1254fae
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-17 20:03:15 +0300

    Add read update and delete message function

--- frontend/src/app/routed/main/pages/direct/direct.page.ts
+++ frontend/src/app/routed/main/pages/direct/direct.page.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Info} from '../../../../features/profile/models/info.model';
-import {Message, MessageInfo, NewMessageRequest} from '../../../../features/chat/models/message.model';
+import {Message, MessageInfo, MessageStatus, MessageUpdate, NewMessageRequest} from '../../../../features/chat/models/message.model';
 import {MessengerSocketService} from '../../../../features/chat/services/messenger-socket.service';
 import {MessengerApiService} from '../../../../features/chat/services/messenger-api.service';
 import {UserApiService} from '../../../../features/profile/services/user-api.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import * as UIkit from 'uikit';
 
 @Component({
   templateUrl: './direct.page.html',
@@ -16,6 +17,7 @@ export class DirectPage implements OnInit {
   messages: MessageInfo[] = [];
   newMessage: string;
   companionInfo: Info;
+  selectedMessage: MessageInfo;
 
   constructor(
     private readonly msgSocketService: MessengerSocketService,
@@ -38,9 +40,24 @@ export class DirectPage implements OnInit {
   subscribeAllMessage(): void {
     this.msgSocketService.getSendMessage().subscribe((message) => {
       this.messages.unshift(message);
+      if (!this.isMyMessage(message)){
+        this.readMessage(message);
+      }
     });
 
-    this.msgSocketService.getDirectException().subscribe((exception) => console.log(exception.error));
+    this.msgSocketService.getReadMessage().subscribe((message) =>
+      this.messages.find(msg => msg.message.id === message.message.id).message.status = MessageStatus.RECIEVED);
+
+    this.msgSocketService.getDeleteMessage().subscribe((message) =>
+      this.messages = this.messages.filter((msg) => msg.message.id !== message.message.id));
+
+    this.msgSocketService.getUpdateMessage().subscribe((message) => {
+      const updateMsgIndex = this.messages.findIndex((msg) => msg.message.id === message.message.id);
+      this.messages[updateMsgIndex] = message;
+    });
+
+    this.msgSocketService.getDirectException().subscribe((exception) =>
+      console.log('SOCKET exception', exception.error.toString()));
   }
 
   getSelectedCompanion(companionId: number): void {
@@ -59,9 +76,16 @@ export class DirectPage implements OnInit {
     this.msgApiService.getDirectMessages(this.companionInfo.id).subscribe(
       (messages) => {
         this.messages = messages;
+        this.readMessages();
       },
       (error) => console.log(error)
     );
+  }
+
+  readMessages(): void {
+    this.messages
+      .filter(msg => !this.isMyMessage(msg))
+      .forEach(msg => this.readMessage(msg));
   }
 
   isMyMessage(message: MessageInfo): boolean {
@@ -72,9 +96,33 @@ export class DirectPage implements OnInit {
     return this.msgApiService.formatDate(message, 'dd.MM HH:mm');
   }
 
+  messageClick(message: MessageInfo): void {
+    if (this.isMyMessage(message)){
+      this.selectedMessage = message;
+      UIkit.modal('#update-delete-panel').show();
+    }
+  }
+
+  deleteSelectedMessage(): void {
+    UIkit.modal('#update-delete-panel').hide();
+    if (this.selectedMessage == null) {
+      return;
+    }
+    this.msgSocketService.directDeleteMessage(this.selectedMessage.message.id);
+  }
+
+  updateSelectedMessage(text: string): void {
+    UIkit.modal('#update-delete-panel').hide();
+    if (text.trim() === ''){
+      return;
+    }
+    const updatedMessage = new MessageUpdate();
+    updatedMessage.id = this.selectedMessage.message.id;
+    updatedMessage.content = text.trim();
+    this.msgSocketService.directUpdateMessage(updatedMessage);
+  }
+
   sendMessage(): void {
-    console.log('тык');
-    console.log(this.newMessage);
     if (!this.newMessage) {
       return;
     }
@@ -86,10 +134,18 @@ export class DirectPage implements OnInit {
     this.newMessage = '';
   }
 
+  readMessage(message: MessageInfo): void {
+    this.msgSocketService.directReadMessage(message.message.id);
+  }
+
   isUpdateMessage(message: MessageInfo): boolean {
     return message.message.dateUpdate !== null;
   }
 
   addFiles(): void {}
+
+  isMessageRead(message: MessageInfo): boolean {
+    return message.message.status === MessageStatus.RECIEVED;
+  }
 
 }
