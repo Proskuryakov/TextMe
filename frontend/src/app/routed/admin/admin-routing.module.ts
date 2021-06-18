43f8de15f75a21750f27ee7fdb5a0b30e5dc2f41
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-18 11:47:38 +0300

    Add routing to reports and moderators page

--- frontend/src/app/routed/admin/admin-routing.module.ts
+++ frontend/src/app/routed/admin/admin-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ReportsPage} from './pages/reports/reports.page';
+import {ModeratorsPage} from './pages/moderators/moderators.page';
+import {OnlyAdminGuard} from '../../core/guards/only-admin.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'reports'
+  },
+  {
+    path: 'reports',
+    component: ReportsPage
+  },
+  {
+    path: 'moderators',
+    canActivate: [OnlyAdminGuard],
+    component: ModeratorsPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/routed/admin/admin.module.ts
+++ frontend/src/app/routed/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { ReportsPage } from './pages/reports/reports.page';
+import { ModeratorsPage } from './pages/moderators/moderators.page';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ReportsPage,
+    ModeratorsPage
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
