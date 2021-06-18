cd876106f64b320d39a89a479175b47d915ba8f2
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-18 11:46:18 +0300

    Add empty moderators page

---
+++ frontend/src/app/routed/admin/pages/moderators/moderators.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './moderators.page.html',
+  styleUrls: ['./moderators.page.sass']
+})
+export class ModeratorsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
