70948c7f9c899cb01e4313f12ab865e1a3cdc5f1
Proskuryakov Egor <proskur.egor@yandex.ru>
2021-06-18 11:46:07 +0300

    Add empty reports page

---
+++ frontend/src/app/routed/admin/pages/reports/reports.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './reports.page.html',
+  styleUrls: ['./reports.page.sass']
+})
+export class ReportsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
